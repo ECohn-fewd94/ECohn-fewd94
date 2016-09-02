7d1597e79b84afbc3636a81222f17da55eab0a72
ECohn-fewd94 <planb@usnycsmg3602.global.publicisgroupe.net>
2016-09-01 21:34:43 -0400

    final matchmaker hw

--- class07/first-javascript/script.js
+++ class07/first-javascript/script.js
@@ -1,3 +1,46 @@
 alert("Hi my name is Emily");
 
 document.write("this is some text");
+document.write('<br>');
+
+var num1= 10*10;
+var num2= 100/10;
+var num3= 50-17;
+var num4= 50+17;
+var name= "Emily" + " " + "Cohn";
+
+document.write(num1);
+
+document.write('<br>');
+
+document.write(num2);
+document.write('<br>');
+
+document.write(num3);
+document.write('<br>');
+
+document.write(num4);
+document.write('<br>');
+
+document.write(name);
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(name);
+
+
+function do_something(){
+	console.log("this is working");
+}
+
+do_something();
+
+
+
+
+
+
+
+
